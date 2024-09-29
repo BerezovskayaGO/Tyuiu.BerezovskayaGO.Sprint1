333d85948373e5bdfdc5d56f03074e8a8c13e5a7
Tyuiu.BerezovskayaGO@outlook.com <Tyuiu.BerezovskayaGO@outlook.com>
2024-09-29 13:28:12 +0500

    Task4

---
+++ Tyuiu.BerezovskayaGO.Sprint1.Task4.V2.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.BerezovskayaGO.Sprint1.Task4.V2.Lib
+{
+    public class DataService : ISprint1Task4V2
+    {
+        public double Calculate(double x, double y)
+        {
+            var res = Math.Round((1 / Math.Sqrt(x + 2 * y)), 3);
+            return res;
+        }
+    }
+}
