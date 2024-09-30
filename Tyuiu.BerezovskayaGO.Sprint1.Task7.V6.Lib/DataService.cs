83fc2d7bb7fe563f995d3a32dd71b0735e9e0af6
Tyuiu.BerezovskayaGO@outlook.com <Tyuiu.BerezovskayaGO@outlook.com>
2024-09-30 21:19:34 +0500

    Task7

---
+++ Tyuiu.BerezovskayaGO.Sprint1.Task7.V6.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.BerezovskayaGO.Sprint1.Task7.V6.Lib
+{
+    public class DataService : ISprint1Task7V6
+    {
+        public double Calculate(double x, double y)
+        {
+            var res = Math.Round((Math.Pow((1 + 1 / Math.Pow(x, 2)), x) - 12 * Math.Pow(x, 2) * y), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.BerezovskayaGO.Sprint1.Task7.V6/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.BerezovskayaGO.Sprint1.Task7.V6.Lib;
+namespace Tyuiu.BerezovskayaGO.Sprint1.Task7.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Березовская Г. О. | АСОиУБ - 24-1";
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                                *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                                  *");
+            Console.WriteLine("* Задание #7                                                                               *");
+            Console.WriteLine("* Вариант #6                                                                               *");
+            Console.WriteLine("* Выполнила: Березовская Галина Олеговна | АСОиУБ - 24-1                                   *");
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                 *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение                           *");
+            Console.WriteLine("* по исходным значениям данных, вводимых пользователем.                                    *");
+            Console.WriteLine("*                                                                                          *");
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                         *");
+            Console.WriteLine("********************************************************************************************");
+
+            double x, y;
+
+            Console.WriteLine("Введите значение x: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                               *");
+            Console.WriteLine("********************************************************************************************");
+
+            Console.WriteLine("(1 + 1/x^2) - 12 * x^2 * y = " + ds.Calculate(x, y));
+
+            Console.ReadKey();
+        }
+    }
+}
